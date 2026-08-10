bb48f9ee05407118833d39fe34d3584fac807cec
Yowan <119342735+YowPro@users.noreply.github.com>
2026-08-11 01:14:17 +0200

    Delete src directory

--- src/api/graphql.ts
+++
@@ -1,9 +0,0 @@
-import { graphql } from '@octokit/graphql';
-
-export function createGraphqlClient(token: string) {
-  return graphql.defaults({
-    headers: {
-      authorization: `bearer ${token}`,
-    },
-  });
-}
